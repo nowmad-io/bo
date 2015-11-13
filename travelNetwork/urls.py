6a35e91d31af4e1f2df348297b9552dfbbe387b6
Julien <julien.rougeron@gmail.com>
2015-11-13 01:54:00 +0100

    don't add namespace to an external module

--- travelNetwork/urls.py
+++ travelNetwork/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^client/', include('client.urls', namespace='client')),
-    url(r'^auth/', include('djoser.urls.authtoken', namespace='auth')),
+    url(r'^auth/', include('djoser.urls.authtoken')),
     url(r'^', include('core.urls', namespace='core')),
 ]
