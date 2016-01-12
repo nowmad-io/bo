ab21f1972943e4e176f76daae9942d4ea576c1f6
Julien <julien.rougeron@gmail.com>
2016-01-12 18:33:47 +0100

    add unicode for easier reading in the admin pages

--- core/models.py
+++ core/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 class Location(models.Model):
     longitude = models.FloatField(blank=False, null=False, default=0)
     latitude = models.FloatField(blank=False, null=False, default=0)
+    def __unicode__(self):
+        return u"%i - %i" % (self.longitude, self.latitude)
 
 class Category(models.Model):
-    name = models.CharField(max_length=30)
-
     class Meta:
         ordering = ['name']
+        verbose_name = _('user')
+
+    name = models.CharField(max_length=30)
+
 
 class Review(models.Model):
     title = models.CharField(max_length=30)
@@ -23,3 +28,6 @@ class Review(models.Model):
     #the who fields
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='first_reviewer')
     creation_date = models.DateField(auto_now_add=True)
+
+    def __unicode__(self):
+            return self.title
