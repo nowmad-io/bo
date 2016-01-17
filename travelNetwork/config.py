c9a5e6e32c5a1ea817683934fbd2cd298db6e5d2
Julien <julien.rougeron@gmail.com>
2016-01-17 18:42:52 +0100

    correct version of database config

--- travelNetwork/config.py
+++ travelNetwork/config.py
@@ -37,13 +37,13 @@ DATABASES = {
 }
 
 # Parse database configuration from $DATABASE_URL
-#DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 # Enable Persistent Connections
-#DATABASES['default']['CONN_MAX_AGE'] = 500
+DATABASES['default']['CONN_MAX_AGE'] = 500
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
