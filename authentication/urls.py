8b0232638afcf429816532b4791f27a56a2f17bf
julienr2 <julien.rougeron@nouma.fr>
2017-09-26 16:31:59 +0200

    comment old djoser register overrider

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,5 @@
 from django.conf.urls import include, url
-from . import views
-
 
 urlpatterns = [
-    # url(r'^register/$', views.RegistrationView.as_view(), name='register'),
     url(r'^', include('djoser.urls.authtoken')),
 ]
