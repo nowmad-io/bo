b7309818f1e699be7961f33c298472bf36e83489
julienr2 <julien.rougeron@nouma.fr>
2017-10-10 19:14:53 +0200

    add whitenoise

--- nowmad/wsgi.py
+++ nowmad/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 from socketio import Middleware
 
 from sockets.views import sio
@@ -16,4 +17,4 @@ from sockets.views import sio
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nowmad.settings")
 
 wsgi_application = get_wsgi_application()
-application = Middleware(sio, wsgi_application)
+application = Middleware(sio, DjangoWhiteNoise(wsgi_application)) 
