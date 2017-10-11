1bb493f9ca9fa22382c574035af8a41d30562c2c
julienr2 <julien.rougeron@nouma.fr>
2017-10-11 13:45:57 +0200

    get default_port from env var

--- nowmad/settings.py
+++ nowmad/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-DEFAULT_PORT = 5000
+DEFAULT_PORT = int(os.environ.get('DEFAULT_PORT', '5000'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
