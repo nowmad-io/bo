e5fd02ea6133b0db1785957b18a798bc444b8eec
julienr2 <julien.rougeron@nouma.fr>
2017-10-11 13:45:43 +0200

    remove sockets urls

--- nowmad/urls.py
+++ nowmad/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'', include('sockets.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('core.urls')),
 ]
