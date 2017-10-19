7352b4e8c3ca06d3a975fc0da4297c88a7e357fe
julienr2 <julien.rougeron@nouma.fr>
2017-10-19 15:13:55 +0200

    custom me endpoint

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
-from .views import RegistrationView
+from .views import RegistrationView, MeView
 
 urlpatterns = [
     url(r'^register/$', RegistrationView.as_view({'post': 'create'}), name='register'),
+    url(r'^me/$', MeView.as_view({'get': 'retrieve'}), name='me'),
     url(r'^', include('djoser.urls.authtoken')),
 ]
