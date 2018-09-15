d5ec655015587681c4904622cc2ac6f03d8c583d
julienr2 <julien.rougeron@gmail.com>
2018-09-14 17:51:46 -0700

    update User Profile

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -4,9 +4,20 @@ from rest_framework import serializers
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.CharField(read_only=True)
+    public_default = serializers.CharField(read_only=True)
+
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name', 'email', 'picture', 'public_default')
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.picture = validated_data.get('picture', instance.picture)
+        instance.save()
+
+        return instance
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,9 @@ from .views import RegistrationView, MeView
 
 urlpatterns = [
     url(r'^register/$', RegistrationView.as_view({'post': 'create'}), name='register'),
-    url(r'^me/$', MeView.as_view({'get': 'retrieve'}), name='me'),
+    url(r'^me/$', MeView.as_view({
+        'get': 'retrieve',
+        'put': 'update'
+    }), name='me'),
     url(r'^', include('djoser.urls.authtoken')),
 ]
