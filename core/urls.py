e036be34e64d6e5567b6feaac2c0cdc9bd3ab2c9
julienr2 <julien.rougeron@gmail.com>
2018-09-21 23:27:10 -0700

    put back notifyme !

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import include, url
 from rest_framework import routers
 
-from core.views import ReviewViewSet, CategoryViewSet, PlaceListView, ReviewPicturesViewSet
+from core.views import ReviewViewSet, CategoryViewSet, PlaceListView, ReviewPicturesViewSet, NotifyMe
 
 router = routers.DefaultRouter()
 router.register(r'reviews', ReviewViewSet, 'reviews')
 router.register(r'categories', CategoryViewSet, 'categories')
 
 urlpatterns = [
+    url(r'^notifyme/$', NotifyMe.as_view()),
     url(r'^places/$', PlaceListView.as_view(), name='placesList'),
     url(r'^reviews/(?P<pk>[a-zA-Z0-9]+)/pictures/$', ReviewPicturesViewSet.as_view({'put': 'update'}), name='Review-pictures'),
     url(r'^auth/', include('authentication.urls')),
